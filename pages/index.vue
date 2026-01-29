741c4dc0718dd60a8743ca6c97ae1fad3dc9dbb8
Sam Li <weisen.li@hotmail.com>
2026-01-29 20:17:12 +0000

    initial project for translations

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onMounted } from 'vue'
+const router = useRouter()
+definePageMeta({ middleware: [] })
+
+onMounted(() => {
+  router.replace('/zh')
+})
+</script>
+
+<template>
+  <div></div>
+</template>
---
+++ plugins/i18n.ts
@@ -0,0 +1,14 @@
+import { createI18n } from 'vue-i18n'
+import en from '../locales/en.json'
+import zh from '../locales/zh.json'
+import { defineNuxtPlugin } from 'nuxt/app'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const i18n = createI18n({
+    legacy: false,
+    locale: 'zh',
+    fallbackLocale: 'en',
+    messages: { en, zh }
+  })
+  nuxtApp.vueApp.use(i18n)
+})
