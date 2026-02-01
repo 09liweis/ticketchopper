ebe6a8bae3e9e30dd4f43157cceadfbe4c2721b3
Sam Li <weisen.li@hotmail.com>
2026-02-01 17:38:37 +0000

    add services pag

---
+++ pages/[lang]/services.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useRoute } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+
+definePageMeta({
+  layout: 'default'
+})
+
+const route = useRoute()
+const { locale } = useI18n()
+
+const lang = (route.params.lang === 'en') ? 'en' : 'zh'
+locale.value = lang
+const currentLang = lang
+</script>
+
+<template>
+  <div>
+    <HomeServices :current-lang="currentLang" />
+    <HomeContactForm :current-lang="currentLang" />
+  </div>
+</template>
