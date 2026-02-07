6ad9124ee67191bbcbc372feb77a9353513074da
Sam Li <weisen.li@hotmail.com>
2026-02-07 21:40:43 +0000

    add zoom translation

--- components/CaseGallery.vue
+++ components/CaseGallery.vue
@@ -253,7 +253,7 @@ onUnmounted(() => {
                 <svg v-else class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM13 10H7" />
                 </svg>
-                <span class="text-sm hidden md:inline">{{ isZoomed ? 'Zoom Out' : 'Zoom In' }}</span>
+                <span class="text-sm hidden md:inline">{{ isZoomed ? t('caseGallery.zoomOut') : t('caseGallery.zoomIn') }}</span>
               </button>
             </div>
 
