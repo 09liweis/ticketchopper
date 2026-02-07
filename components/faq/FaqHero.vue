491b4a729baea2b92b351be9f4a9098ac7129358
Sam Li <weisen.li@hotmail.com>
2026-02-07 16:18:36 +0000

    fix faq home link

--- components/faq/FaqHero.vue
+++ components/faq/FaqHero.vue
@@ -16,9 +16,9 @@ const { t } = useI18n()
     <div class="absolute inset-0 opacity-10"></div>
     <div class="container mx-auto px-4 relative z-10">
       <nav class="flex items-center space-x-2 text-blue-200 mb-6">
-        <a :href="`/${currentLang}`" class="hover:text-white transition-colors">
+        <NuxtLink :to="`/${currentLang}`" class="hover:text-white transition-colors">
           {{ t('common.home') }}
-        </a>
+        </NuxtLink>
         <svg class="w-4 h-4 fill-current" viewBox="0 0 24 24">
           <path d="M8.59 16.58L10 18l6-6-6-6-1.41 1.41L13.17 12l-4.58 4.58z"/>
         </svg>
