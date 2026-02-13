7d4ff63e80995b51f19921094918e7fd8269a886
Sam Li <weisen.li@hotmail.com>
2026-02-13 16:55:00 +0000

    update contact page seo

--- pages/[lang]/contact.vue
+++ pages/[lang]/contact.vue
@@ -1,17 +1,23 @@
 <script setup>
 import { useRoute } from 'vue-router'
 import { useI18n } from 'vue-i18n'
+import { useSeo } from '~/composables/useSeo'
 
 definePageMeta({
   layout: 'default'
 })
 
 const route = useRoute()
-const { locale } = useI18n()
+const { locale, t } = useI18n()
 
 const lang = (route.params.lang === 'en') ? 'en' : 'zh'
 locale.value = lang
 const currentLang = lang
+
+useSeo({
+  title: `${t('contact.title')} - ${t('meta.title')}`,
+  description: t('contact.desc')
+})
 </script>
 
 <template>
