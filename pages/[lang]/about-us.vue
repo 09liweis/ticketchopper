a14866a7555cedacd92e93d6fa9439239c93bcc1
Sam Li <weisen.li@hotmail.com>
2026-02-13 16:54:48 +0000

    update about us page seo

--- pages/[lang]/about-us.vue
+++ pages/[lang]/about-us.vue
@@ -1,17 +1,23 @@
 <script setup>
 import { useRoute } from 'vue-router'
 import { useI18n } from 'vue-i18n'
+import { useSeo } from '~/composables/useSeo'
 
 definePageMeta({
   layout: 'default'
 })
 
 const route = useRoute()
-const { locale } = useI18n()
+const { locale, t } = useI18n()
 
 const lang = (route.params.lang === 'en') ? 'en' : 'zh'
 locale.value = lang
 const currentLang = lang
+
+useSeo({
+  title: `${t('nav.about')} - ${t('meta.title')}`,
+  description: t('whyChooseUs.description')
+})
 </script>
 
 <template>
