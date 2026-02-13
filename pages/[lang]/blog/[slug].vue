597b7611dcd5a1ab14807260cb6d2455d9215748
Sam Li <weisen.li@hotmail.com>
2026-02-13 16:55:37 +0000

    get blog page seo

--- pages/[lang]/blog/[slug].vue
+++ pages/[lang]/blog/[slug].vue
@@ -3,6 +3,7 @@ import { useRoute } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { computed } from 'vue'
 import { useBlogPosts } from '~/composables/useBlogPosts'
+import { useSeo } from '~/composables/useSeo'
 
 definePageMeta({
   layout: 'default'
@@ -20,11 +21,25 @@ const blogSlug = route.params.slug
 
 const currentPost = computed(() => {
   const post = getPostBySlug(String(blogSlug), currentLang)
+  if (post) {
+    useSeo({
+      title: `${post.title} - ${t('meta.title')}`,
+      description: post.excerpt,
+      blogPost: post
+    })
+  } else {
+    useSeo({
+      title: t('common.blogNotFound'),
+      description: ''
+    })
+  }
+
   return post || {
     title: t('common.blogNotFound'),
-    date: '01 Jan 2026',
+    date: { day: '01', month: 'Jan' },
     author: 'seagod',
     category: t('common.general'),
+    excerpt: '',
     fullContent: currentLang === 'en' 
       ? '<p>This blog post could not be found.</p>' 
       : '<p>找不到此博客文章。</p>'
