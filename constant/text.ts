a64f3049c9f942f9d3855cd0a599cc50a0525224
Sam Li <weisen.li@hotmail.com>
2026-02-14 21:10:38 +0000

    update phone number

--- constant/text.ts
+++ constant/text.ts
@@ -1,4 +1,4 @@
-export const PHONE_NUMBER = '289-809-8899';
+export const PHONE_NUMBER = '+1-365-739-8928';
 export const WECHAT_NUMBER = '1020613826';
 export const WECHAT_QR = '/images/wechat.jpg';
-export const COMPANY_ADDRESS = '203-1943 Ironoak Way Oakville ON L6H 3V7';
+export const COMPANY_ADDRESS = 'TBA';
