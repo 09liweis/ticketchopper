f3d1912b44bf504429a2279f98ee3b38dac20418
Sam Li <weisen.li@hotmail.com>
2026-02-15 16:21:55 +0000

    update wechat number

--- constant/text.ts
+++ constant/text.ts
@@ -1,4 +1,4 @@
 export const PHONE_NUMBER = '+1-365-739-8928';
-export const WECHAT_NUMBER = '1020613826';
+export const WECHAT_NUMBER = 'wenshenglegal';
 export const WECHAT_QR = '/images/wechat.jpg';
 export const COMPANY_ADDRESS = 'TBA';
